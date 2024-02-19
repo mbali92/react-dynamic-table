5edc03c7e19c6ee6f4a9effb75dddd7771039c74
santos rafaelo <santoschrist1234@gmail.com>
2024-02-19 16:13:15 +0200

    feat:modified handleData and handleEdit methods

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,32 +1,32 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./styles/style.css"
+import axios from 'axios';
 
 function Table() {
 
+  
+
+ const [data, setData] = useState({});
 
 
- const [data, setData] = useState([{
-  title:'title1',
-  description:'description',
-  owner:'owner',
-  status:'status',
-  startDate:'12-12-12',
-  endDate:'12-12-12',
-  priority:'low',
-  comment:'comment'
- },
- {
-  title:'title2',
-  description:'description',
-  owner:'owner',
-  status:'status',
-  startDate:'12-12-12',
-  endDate:'12-12-12',
-  priority:'critical',
-  comment:'comment'
- }]);
 
- 
+ const allData = async () => {
+  
+  try {
+    const newData = await axios.get('http://localhost:8080/getAllTask')
+    console.log("succes")
+    setData(newData.data);
+    console.log("db data")
+  } catch (error) {
+    console.log(error)
+  }
+
+ }
+//  useEffect(()=>{
+//     console.log(data)
+//  },[data])
+
+
 
    const addRow=()=>{
       const singlerow= {
@@ -43,86 +43,40 @@ function Table() {
 
    }
 
-  //  const handleDelete = (title) =>{
-  //   const newfields  = data.filter((filter)=> filter.title !== title);
-  //   setData(newfields)
-  //  }
-
-
-  
-   const handleDuplicate =(title)=>{
-
-    const findRow = data.find((find)=>find.title === title);
-          const singlerow= {
-            title:findRow.title,
-            description:findRow.description,
-            owner:findRow.owner,
-            status:findRow.status,
-            startDate:findRow.startDate,
-            endDate:findRow.endDate,
-            priority:findRow.priority,
-            comment:findRow.comment
-            }
-setData(prevArray => [...prevArray, singlerow])
+   const handleDelete = (title) =>{
+    const newfields  = data.filter((filter)=> filter.title !== title);
+    setData(newfields)
    }
 
 
-const handleEdit = (e, title) => {
-  const propertyName = e.target.name;
-  const propertyValue = e.target.value;
-
-  // Find the object in the data array
+const handleEdit = (e, title,name) => {
+  
+  const propertyValue = e.target.innerText;
   const updatedData = data.map(item => {
     if (item.title === title) {
-      // Create a new object with the updated property
-      return { ...item, [propertyName]: propertyValue };
+      
+      return { ...item, [name]: propertyValue };
     }
     return item;
   });
 
-
-
-  // Update the state with the new array
-  setData(updatedData);
-
-  // Log the updated data after the state has been updated
-  // console.log(updatedData);
-  // console.log(updatedData1);
-
-
-  // Since data is updated asynchronously, logging data immediately will not reflect the updated state
-  console.log(data);
+  console.log(updatedData); 
+ 
 };
 
 
-
-
-
-  // console.log(data)
-   const accessSelectValue = (e,title)=>{
-    const demo = data.find((find)=>find.title===title)
-    demo.status = e.target.value;
-    setData(data)
-    // console.log(data)
-   }
-
-
-
-   const prioritySelectValue = (e,title)=>{
-    const demo = data.find((find)=>find.title===title)
-    demo.priority = e.target.value;
-    setData(data)
-    console.log(data)
-   }
-
-
-
-   
+const handleData=(e,index)  => {
+  const name = e.target.name;
+  const newValue = e.target.value;
+  const dateObjects = [...data]
+  dateObjects[index][name] = newValue;
+  setData(dateObjects)
+};
 
    
 
   return (
-    <>
+
     <div className="container">
        <div className="table">
         <div className="field-row  page-row">
@@ -130,72 +84,63 @@ const handleEdit = (e, title) => {
           <div className='field-name column-field'>Title</div>
           <div className='field-name'>Description</div>
           <div className='field-name'>Owner</div>
-          <div className='field-name'>Due-Date</div>
           <div className='field-name'>Start-Date</div>
+          <div className='field-name'>Due-Date</div>
           <div className='field-name'>Priority</div>
           <div className='field-name'>Status</div>
           <div className='field-name'>Comment</div>
-        </div>
-        {data.map((data)=>
-        <div className="field-row  page-row"key={data.title}>
-          <div className="row-option" onClick={()=>handleDuplicate(data.title)}><i className="lni lni-more-alt"></i></div>
-
-          {/* <div className='field-name row-data'> */}
-          <input type='text' className='field-name row-data edit-value' name='title' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.title}/>
-          {/* </div> */}
-
-          {/* <div className='field-name row-data'> */}
-            <input type='text' className='field-name row-data edit-value' name='description' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.description}/>
-          {/* </div> */}
-
-          {/* <div className='field-name row-data'> */}
-            <input type='text' className='field-name row-data edit-value' name='owner' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.owner}/>
-          {/* </div> */}
-        
-
-          {/* <div  className='field-name row-data'> */}
-          <input type='text' className='field-name row-data edit-value' name='startDate' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.startDate}/>
-          {/* </div> */}
-          <input type='text' className='field-name row-data edit-value' name='endDate' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.endDate} />
           
-          {/* <div className='field-name row-data'> */}
-          <input type='text' className='field-name row-data edit-value' name='priority' onChange={(e) =>  handleEdit(e,data.title)} defaultValue={data.priority} />
-         
+        </div>
+        {data.map((data,index)=>
+        <div className="field-row  page-row"key={data.title}>
+          <div className="row-option" ><i className="lni lni-more-alt"></i></div>
+          <div className='field-name row-data edit-value'  contentEditable onInput={(e)=>handleEdit(e,data.title,"title")} ><h6>{data.title}</h6></div>
+          <div className='field-name row-data edit-value'  contentEditable onInput={(e)=>handleEdit(e,data.title,"description")} ><h6>{data.description}</h6></div>
+          <div className='field-name row-data edit-value'  contentEditable onInput={(e)=>handleEdit(e,data.title,"owner")} ><h6>{data.owner}</h6></div>
+          <input type='date' className='field-name row-data edit-value' name='startDate' onChange={(e) =>  handleData(e,index)} defaultValue={data.startDate} />
+          <input type='date' className='field-name row-data edit-value' name='endDate' onChange={(e) =>  handleData(e,index)} defaultValue={data.endDate} />
+  
           <div className='field-name row-data'>
-            <select name="status" id="" onChange={(e)=>prioritySelectValue(e, data.title)}>
+          <select name="priority" id="select"  onChange={(e)=>handleData(e, index)}>
               <option value="low">low</option>
               <option value="medium">medium</option>
               <option value="critical">critical</option>
             </select>
+         </div>
+          <div className='field-name row-data'>
+            <select name="status" id="" onChange={(e)=>handleData(e, index)}>
+              <option id="todo" value="todo">todo</option>
+              <option id="progress" value="in progress">in progress</option>
+              <option id="done" value="done">done</option>
+            </select>
           </div>
+         
+          <div className='field-name row-data edit-value'  contentEditable onInput={(e)=>handleEdit(e,data.title,"comment")}><h6>{data.comment}</h6></div>          
+          
+          
+
+
+
 
          
           {/* </div> */}
 
-          <div className='field-name row-data'>
-            <select name="status" id="" onChange={(e)=>accessSelectValue(e, data.title)}>
-              <option value="todo">todo</option>
-              <option value="in progress">in progress</option>
-              <option value="done">done</option>
-            </select>
-          </div>
+    
           
-          <div className='field-name row-data'  >
-          <input type='text' className='field-name row-data edit-value' name='comment' onChange={(e) =>  handleEdit(e,data.comment)} defaultValue={data.comment} />
-
           
-          </div>
           
         </div>)
         } 
        </div>
-       <button onClick={ addRow}>Add Row</button>
+       <button onClick={ ()=>allData()}>Add Row</button>
 
     </div>
       
     
-    </>
+   
   )
 }
 
 export default Table
+
+
